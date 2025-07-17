ba5b6db95bf21592e55971496c5e0ff0b4aedf77
Annie Huang <annie.hn@gmail.com>
2025-07-17 22:06:30 +1000

    Create <Container> in App.tsx and Home.tsx for the <Card>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Home } from './Home';
+import { Box, Container } from '@mui/material';
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Box marginTop={10}>
+        <Home />
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+
+export const Home = () => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h1'>Hello Youtube!!!</Typography>
+      </CardContent>
+    </Card>
+  );
+};
