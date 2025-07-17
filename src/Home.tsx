c3fd98d62dae72c4a888ed9192c5b7c220938b4b
Annie Huang <annie.hn@gmail.com>
2025-07-17 22:15:16 +1000

    Add <Formik> <Form> <Field> into Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import { Field, Form, Formik, FormikHelpers, FormikValues } from 'formik';
+import { TextField } from 'formik-mui';
 
 export const Home = () => {
   return (
     <Card>
       <CardContent>
-        <Typography variant='h1'>Hello Youtube!!!</Typography>
+        {/*<Typography variant='h1'>Hello Youtube!!!</Typography>*/}
+        <Formik
+          initialValues={{}}
+          onSubmit={function (
+            values: FormikValues,
+            formikHelpers: FormikHelpers<FormikValues>,
+          ): void | Promise<any> {
+            throw new Error('Function not implemented.');
+          }}
+        >
+          <Form>
+            <Field name='firstName' component={TextField} label='First Name' />
+          </Form>
+        </Formik>
       </CardContent>
     </Card>
   );
