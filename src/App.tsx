7a7eaa34d7d91f8949152e97a6ef8c2aad8d0962
Annie Huang <annie.hn@gmail.com>
2025-07-17 22:10:41 +1000

    Add ThemeProvider and global reset from material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { Home } from './Home';
-import { Box, Container } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
+import { theme } from './theme';
 
 function App() {
   return (
-    <Container>
-      <Box marginTop={10}>
-        <Home />
-      </Box>
-    </Container>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <AppBar position='fixed'>
+          <Toolbar variant='dense'>
+            <Typography variant='h6'>Multi-Step Form</Typography>
+          </Toolbar>
+        </AppBar>
+
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Container>
+          <Box marginTop={10}>
+            <Home />
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
