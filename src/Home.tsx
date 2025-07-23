7b4e5490088920f190fc2a0eb14438af22baede9
Annie Huang <annie.hn@gmail.com>
2025-07-23 21:09:28 +1000

    Choose the standard variant for the TextField

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,8 +37,18 @@ export const Home = () => {
           }}
         >
           <Form>
-            <Field name='firstName' component={TextField} label='First Name' />
-            <Field name='lastName' component={TextField} label='Last Name' />
+            <Field
+              name='firstName'
+              component={TextField}
+              label='First Name'
+              variant='standard'
+            />
+            <Field
+              name='lastName'
+              component={TextField}
+              label='Last Name'
+              variant='standard'
+            />
             <Field
               name='millionaire'
               type='checkbox'
@@ -50,11 +60,13 @@ export const Home = () => {
               type='number'
               component={TextField}
               label='All the money I have'
+              variant='standard'
             />
             <Field
               name='description'
               component={TextField}
               label='Description'
+              variant='standard'
             />
           </Form>
         </Formik>
