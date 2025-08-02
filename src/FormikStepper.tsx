e6633d5e02272b431d20393f60d847e95c4d476e
Annie Huang <annie.hn@gmail.com>
2025-08-02 16:47:17 +1000

    Make a backup of the current FormikStepper file

--- src/FormikStepper.tsx
+++ src/FormikStepper.tsx
@@ -79,8 +79,81 @@ export const FormikStepper: FC<FormikConfig<Values>> = ({
       )}
     </Formik>
   );
+};
 
-  /*  return (
+/*
+export const FormikStepper: FC<FormikConfig<Values>> = ({
+  children,
+  ...props
+}) => {
+  // https://github.com/jaredpalmer/formik/issues/3683, if use children directly in the template, you will need to wrap it with <>{children}</>, like the backup below
+  // @ts-ignore
+  // const childrenArray = React.Children.toArray(
+  //   children,
+  // ) as React.ReactElement<FormikStepProps>[];
+  const childrenArray = children as React.ReactElement<FormikStepProps>[];
+
+  const [step, setStep] = useState(0);
+  const currentChild = childrenArray[step];
+  console.log('currentChild', currentChild);
+
+  const isLastStep = () => {
+    return step === childrenArray.length - 1;
+  };
+
+  const onSubmit = async (values: Values, helpers: FormikHelpers<Values>) => {
+    if (isLastStep()) {
+      await props.onSubmit(values, helpers);
+    } else {
+      setStep((s) => s + 1);
+    }
+  };
+
+  return (
+    <Formik
+      {...props}
+      // only the 2nd child got the validationSchema value. After this is added, you cannot move to the 3rd step if you choose millionaire and didn't enter > 1m for money
+      validationSchema={currentChild.props.validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form autoComplete='off'>
+          <Stepper alternativeLabel activeStep={step}>
+            {childrenArray.map((child) => (
+              <Step key={child.props.label}>
+                <StepLabel>{child.props.label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+
+          {currentChild}
+
+          {step > 0 && (
+            <Button
+              disabled={isSubmitting}
+              variant='contained'
+              color='primary'
+              onClick={() => setStep((s) => s - 1)}
+              sx={{ mr: 2 }}
+            >
+              Back
+            </Button>
+          )}
+          <Button
+            disabled={isSubmitting}
+            variant='contained'
+            color='primary'
+            type='submit'
+          >
+            {/!*{isLastStep() ? 'Submit' : 'Next'}*!/}
+            {isSubmitting ? 'Submitting' : isLastStep() ? 'Submit' : 'Next'}
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+
+  /!*  return (
     <Formik
       {...props}
       // only the 2nd child got the validationSchema value. After this is added, you cannot move to the 3rd step if you choose millionaire and didn't enter > 1m for money
@@ -112,8 +185,9 @@ export const FormikStepper: FC<FormikConfig<Values>> = ({
         </Button>
       </Form>
     </Formik>
-  );*/
+  );*!/
 };
+*/
 
 /*
 export const FormikStepper: FC<FormikConfig<Values>> = ({
